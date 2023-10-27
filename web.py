d60b2c2ced518d99bfa235de42ed31b839fce6bd
lorenzo <ozznero@gmail.com>
2023-10-27 11:43:06 +0200

    webapp version of the Todo App - web page simple layout, creating checkboxes for items, automating new item insertion in backend file

--- web.py
+++ web.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
-st.subheader("A webapp for helping people remember and prioritize tasks")
-st.text("Just another website construction kit, this is streamlit on python")
+st.subheader("A webapp for helping people remember tasks")
+st.text("streamlit - Just another website construction kit")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input("", placeholder="Insert a new todo item")
+st.text_input(" ", placeholder="Insert a new todo item",
+              on_change=add_todo, key='new_todo')
+
+print("New roundtrip")
+st.session_state
