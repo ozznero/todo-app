efbee8bafc6b7213529ec268b311e2ba00a08335
lorenzo <ozznero@gmail.com>
2023-10-30 16:25:21 +0100

    webapp version of the Todo App - completed

--- web.py
+++ web.py
@@ -14,11 +14,16 @@ st.title("My Todo App")
 st.subheader("A webapp for helping people remember tasks")
 st.text("streamlit - Just another website construction kit")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(" ", placeholder="Insert a new todo item",
               on_change=add_todo, key='new_todo')
 
-print("New roundtrip")
-st.session_state
+# print("New roundtrip")
+# st.session_state
